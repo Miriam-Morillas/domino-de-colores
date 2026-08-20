c7d4fe962e91a389b6a813ebdda19ffe949ca1bc
DESKTOP-BSC77CE\Usuario <miriam.morillas@xarxatecactiva.com>
2026-08-20 22:36:39 +0200

    Creadas las 28 fichas del dominó con colores aleatorios distintos

--- domino.js
+++ domino.js
@@ -7,3 +7,75 @@ class Ficha {
         this.colorB = colorB;
     }
 }
+const colores = [
+    "red",
+    "yellow",
+    "blue",
+    "green"
+];
+const fichas = [];
+for (let numeroA = 0; numeroA<= 6; numeroA++) {
+    for (
+        let numeroB = numeroA;
+        numeroB <= 6;
+        numeroB++
+
+    ) {
+        const indiceColorA =
+        Math.floor(
+            Math.random() * colores.length 
+        );
+        const colorA =
+        colores[indiceColorA];
+
+        let indiceColorB =
+        Math.floor(
+            Math.random() * colores.length
+        );
+
+        let colorB =
+        colores[indiceColorB];
+
+        while (colorA === colorB) {
+            indiceColorB =
+            Math.floor(
+                Math.random() * colores.length
+            );
+            colorB =
+            colores[indiceColorB];
+        }
+        const nuevaFicha =
+        new Ficha(
+            numeroA,
+            colorA,
+            numeroB,
+            colorB
+        );
+
+        fichas.push(
+            nuevaFicha
+        );
+ 
+    }
+    
+}    
+console.log(
+    "Numero de fichas:",
+    fichas.length
+);
+
+console.log(
+    "Todas las fichas:",
+    fichas
+);
+
+let coloresCorrectos = true;
+for (const ficha of fichas) {
+    if (ficha.colorA === ficha.colorB) {
+        coloresCorrectos = false;
+    }
+}
+console.log(
+    "¿Todas las fichas tienen colores distintos?",
+    coloresCorrectos
+); 
