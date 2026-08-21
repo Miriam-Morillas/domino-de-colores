c6dd70bc15def6882d49d147c4ce6a00c3cd4578
DESKTOP-BSC77CE\Usuario <miriam.morillas@xarxatecactiva.com>
2026-08-21 23:42:36 +0200

    Se añade una función que comprueba si cualquiera de los dos números de una ficha coincide con alguno de los extremos abiertos del tablero.

--- domino.js
+++ domino.js
@@ -284,41 +284,22 @@ if (turno ==="jugador") {
 
 }
 
-console.log(
-    "Jugador que empezó:",
-    turnoInicial
-);
-
-console.log(
-    "Ficha inicial colocada:",
-    tablero[0]
-);
-
-console.log(
-    "Número de fichas en el tablero:",
-    tablero.length
-);
-
-console.log(
-    "Fichas del jugador:",
-    fichasJugador.length
-);
-
-console.log(
-    "Fichas de la máquina:",
-    fichasMaquina.length
-);
-
-console.log(
-    "Turno después de colocar la ficha inicial:",
-    turno
-);
-
 let extremoIzquierdo = 
 tablero[0].numeroA;
 
 let extremoDerecho =
 tablero[0].numeroB;
+
+function esJugable(ficha) {
+
+    return ( 
+        ficha.numeroA === extremoIzquierdo ||
+        ficha.numeroA === extremoDerecho ||
+        ficha.numeroB === extremoIzquierdo ||
+        ficha.numeroB === extremoDerecho
+    )
+       
+}
 
 console.log(
     "Extremo izquierdo:",
@@ -329,3 +310,11 @@ console.log(
     "Extremo derecho:",
     extremoDerecho
 );
+
+for (const ficha of fichasJugador) {
+    console.log(
+        `${ficha.numeroA}-${ficha.numeroB}`,
+        "¿Jugable?",
+        esJugable(ficha)
+    );
+}
